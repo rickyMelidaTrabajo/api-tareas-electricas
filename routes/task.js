2a5a1d6eb949eeed63e427f7acbdf62bb2fbb3c7
RickyMelidaTrabajo <ricardomelida92.trabajo@gmail.com>
2021-06-13 11:41:57 -0400

    Se agregan rutas para obtener tareas

--- routes/task.js
+++ routes/task.js
@@ -8,5 +8,7 @@ const md_upload = multipart({ uploadDir: './upload' });
 
 router.post('/add-pending-task', auth, md_upload, taskController.addPendingTask);
 router.post('/add-finished-task', md_upload, auth, taskController.addFinishedTask);
+router.get('/get-pending-tasks', auth, taskController.showPendingTasks);
+router.get('/get-finished-tasks', auth, taskController.showFinishedTasks);
 
 module.exports = router;
