cfd1358030f106739739d017ffef3b5463f42f67
Ricardo Melida <ricardomelida92@gmail.com>
2021-06-15 21:51:17 -0400

    Se agrega cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const cors = require('cors');
 
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use(session({
     key: 'user',
     secret: 'sacdvfnnfvf1',
